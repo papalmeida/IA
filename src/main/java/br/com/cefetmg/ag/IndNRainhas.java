599c187da799a78ccf798e783d3fbc08365167e2
papalmeida <pedro.augusto120105@gmail.com>
2026-04-21 16:04:23 -0300

    feat: preencher genes com valores aleatórios

--- src/main/java/br/com/cefetmg/ag/IndNRainhas.java
+++ src/main/java/br/com/cefetmg/ag/IndNRainhas.java
@@ -2,16 +2,22 @@ package br.com.cefetmg.ag;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class IndNRainhas implements Individuo {
     private double txMutacao = 0.3;
     private int[] genes;
     private int qtdGenes;
     private boolean maximizacao = false;
+    private static Random linhaRainha;
 
     public IndNRainhas(int qtdGenes) {
-        this.qtdGenes = qtdGenes;
         this.genes = new int[qtdGenes];
+        linhaRainha = new Random();
+        for(int i =0; i < qtdGenes; i++) {
+            genes[i] = linhaRainha.nextInt(0,qtdGenes);
+        }
+        this.qtdGenes = qtdGenes;
     }
 
     public IndNRainhas(int qtdGenes, int[] genes) {
