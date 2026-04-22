8e3e6e6924f3b704f65e87964cf3e8f16b9b2db6
papalmeida <pedro.augusto120105@gmail.com>
2026-04-21 23:32:02 -0300

    fix: remoção do print e variável inutilizada no AgMain

--- src/main/java/br/com/cefetmg/ag/AgMain.java
+++ src/main/java/br/com/cefetmg/ag/AgMain.java
@@ -2,9 +2,8 @@ package br.com.cefetmg.ag;
 
 public class AgMain {
     public static void main(String[] args) throws Exception {
-        IndNRainhasFactory factory = new IndNRainhasFactory(20);
+        IndNRainhasFactory factory = new IndNRainhasFactory(10);
         Ag algoritmoGenetico = new Ag();
-        Individuo melhor = algoritmoGenetico.executar(factory, 30, 6, 3000);
-        System.out.println("\nMelhor Individuo da N-Rainhas:\n" + melhor);
+        algoritmoGenetico.executar(factory, 4, 1, 10000);
     }
 }
